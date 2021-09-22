b61d4c51f42e4896dc8a690becdd3e0c4eb85800
Ben Cartner <ben.cartner@gmail.com>
2021-09-22 16:32:56 +1200

    remove single str clause

--- predictor_src/predictor.py
+++ predictor_src/predictor.py
@@ -28,13 +28,10 @@ def predictor(json_strings, model_path="model.ckpt"):
     encoder = LitAutoEncoder()
     encoder.load_from_checkpoint(model_path)
     trainer = pl.Trainer(gpus=None, logger=False)
-    is_single = None
 
     if isinstance(json_strings, str):
         json_strings = [json_strings]
-        is_single = True
-    else:
-        is_single = False
+        
     test_dataset = loader(json_strings)
 
     data_loader = torch.utils.data.DataLoader(
@@ -42,16 +39,12 @@ def predictor(json_strings, model_path="model.ckpt"):
 
     predictions = trainer.predict(encoder, data_loader)
 
-    if is_single:
-        return_val = torch.Tensor.tolist(predictions[0])
-        return_val = {'label':labels[return_val.index(max(return_val))],
-                      'confidence':"{:.2%}".format(max(return_val))}
-    else:
-        vals = list(map(lambda x: torch.Tensor.tolist(x), predictions))
-        top_vals = [max(x) for x in vals]
-        top_vals_indicies = [val_entry.index(
+    
+    vals = list(map(lambda x: torch.Tensor.tolist(x), predictions))
+    top_vals = [max(x) for x in vals]
+    top_vals_indicies = [val_entry.index(
             x) for x, val_entry in zip(top_vals, vals)]
-        return_val = [{"label":LABELS[index],"confidence":"{:.2%}".format(top_val[index])} 
+    return_val = [{"label":LABELS[index],"confidence":"{:.2%}".format(top_val[index])} 
                       for index,top_val in zip(top_vals_indicies,top_vals)]
 
     return return_val  # Lambda instance will stringify it for us.
